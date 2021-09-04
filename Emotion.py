8a5cdeed1bdbfc61cd8588b2069f168a23e1b04a
Robin <60317828+Robin-WZQ@users.noreply.github.com>
2021-09-04 09:58:32 +0800

    Update Emotion.py

--- Emotion.py
+++ Emotion.py
@@ -38,11 +38,19 @@ from main import *
 from Video_main import *
 from face_detect import show_face
 import time
+import pyaudio
+
+from real_time_processing_v2 import real_time_processing,real,init
+from tools.picture_capture import capture
+from tools.write_wav import record
+
+
 class Ui_MainWindow(QMainWindow):
     def __init__(self,MainWindow):
         super(Ui_MainWindow, self).__init__()
         self.path = getcwd()
         self.timer_camera = QtCore.QTimer(self) # 定时器
+        self.timer_camera2 = QtCore.QTimer(self)
 
         self.setupUi(MainWindow)
         self.retranslateUi(MainWindow)
@@ -247,7 +255,6 @@ class Ui_MainWindow(QMainWindow):
         self.label.setFont(font)
         self.label.setObjectName("label")
 
-
         self.line_2 = QtWidgets.QFrame(self.centralwidget)
         self.line_2.setGeometry(QtCore.QRect(1090, 330, 421, 16))
         self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
@@ -297,6 +304,7 @@ class Ui_MainWindow(QMainWindow):
         self.pushButton_pause.clicked.connect(self.button_pause)
         self.pushButton_close.clicked.connect(QCoreApplication.quit)
         self.timer_camera.timeout.connect(self.show_camera)
+        self.timer_camera2.timeout.connect(self.show_face2)
 
     def aaa(self,x):
         xx=str(x)
@@ -361,7 +369,6 @@ class Ui_MainWindow(QMainWindow):
         #后加的，从实时转到播放
 
         self.timer_camera.stop()
- 
         self.cap.release()
         self.label_face.clear()
         #gif = QMovie(':/newPrefix/images_test/scan.gif')
@@ -441,7 +448,6 @@ class Ui_MainWindow(QMainWindow):
         self.pushButton_pause.setEnabled(True)
         self.pushButton_close.setEnabled(True)
 
-
         name = video_record()
         path = "D:/research/dachuang/project/emotion/muti-modal emotion recognition/Emotion_rec/"+name+".mp4"
         show_face(path)
@@ -479,10 +485,47 @@ class Ui_MainWindow(QMainWindow):
     #    self.lab_video.setText(str(round((position/self.vidoeLength)*100,2))+'%')
     #    self.timePlay = str(round((position/self.vidoeLength)*100,2))+'%'
     
+    def init2(self) -> str:
+        print("正在初始化相关配置")    
+        name = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        dst_dir_path = 'data/Joy'
+        if not os.path.exists(dst_dir_path):
+            os.mkdir(dst_dir_path)
+        if not os.path.exists(dst_dir_path+"/"+name):
+            os.mkdir(dst_dir_path+"/"+name)
+        if not os.path.exists(dst_dir_path+"/"+name+"/images/"):
+            os.mkdir(dst_dir_path+"/"+name+"/images/")
+        if not os.path.exists(dst_dir_path+"/"+name+"/mp3/"):
+            os.mkdir(dst_dir_path+"/"+name+"/mp3/")
+            os.mkdir(dst_dir_path+"/"+name+"/mp3/mp3/")
+        images_path = dst_dir_path+"/"+name+"/images/"
+
+        n_frame_fix(name)
+        rewrite_josn(name)
+        json_processing()
+        image_path = "data/Joy/"+name
+        audio_path = "data/Joy/"+name+"/mp3/mp3/"
+        log_dir = "save_30.pth"
+
+        opt = parse_opts()
+        opt.device_ids = list(range(device_count()))
+        local2global_path(opt)
+        model, parameters = generate_model(opt)
+
+        criterion = get_loss(opt)
+        criterion = criterion.cuda()
+        optimizer = get_optim(opt, parameters)
+
+        writer = SummaryWriter(logdir=opt.log_path)
+        print("配置结束")
+        return name,images_path,image_path,audio_path,log_dir,model,criterion,optimizer,writer
 
     #打开摄像头  实时分析
     def button_open_camera_click(self):
+        self.label_face.clear()
         if self.timer_camera.isActive() == False: # 检查定时状态
+            self.pushButton_close.setEnabled(True)
+            self.label_face.setText('正在启动识别系统...\n\nleading')            
             flag = self.cap.open(self.CAM_NUM) # 检查相机状态
             if flag == False: # 相机打开失败提示
                 msg = QtWidgets.QMessageBox.warning(self.centralwidget, u"Warning",
@@ -492,15 +535,18 @@ class Ui_MainWindow(QMainWindow):
             else:
                 # 准备运行识别程序
                 QtWidgets.QApplication.processEvents()
-                self.label_face.setText('正在启动识别系统...\n\nleading')
-                # 新建对象
-                #self.emotion_model = Emotion_Rec(self.model_path)
+                self.my_camera = cv2.VideoCapture(0)
+                name,self.images_path,self.image_path,self.audio_path,self.log_dir,self.model,self.criterion,self.optimizer,self.writer=self.init2()
+                self.label_face.setText('识别系统启动成功...\n\nleading')
                 QtWidgets.QApplication.processEvents()
-                # 打开定时器
-                self.timer_camera.start(30)
+                t0 = Thread(target=self.show_face2)
+                t0.start()
+                t1 = Thread(target = self.show_camera)
+                t1.start()
         else:
             # 定时器未开启，界面回复初始状态
             self.timer_camera.stop()
+            self.timer_camera2.stop()
             self.cap.release()
             self.label_face.clear()
             gif = QMovie(':/newPrefix/images_test/scan.gif')
@@ -511,11 +557,68 @@ class Ui_MainWindow(QMainWindow):
 
             self.label_scanResult.setText('None')
 
+    def show_face2(self):
+        detector = dlib.get_frontal_face_detector()
+        while True:
+            flag, image = self.my_camera.read() # 获取画面
+            image=cv2.flip(image, 1) # 左右翻转
+            frameClone = cv2.resize(image,(420,280))
+            # 在Qt界面中显示人脸
+            show = cv2.cvtColor(frameClone, cv2.COLOR_BGR2RGB)
+            faces = detector(show)
+            for face in faces:   
+                x1 = face.left()    
+                y1 = face.top()    
+                x2 = face.right()    
+                y2 = face.bottom()    
+                cv2.rectangle(img=show, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=4)
+                break
+            showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
+            self.label_face.setPixmap(QtGui.QPixmap.fromImage(showImage))
+            QtWidgets.QApplication.processEvents()
+    
+    def show_emotion_picture(self,result,preds):
+        tmp = open('slice.png', 'wb')
+        tmp.write(b64decode(bgImg))
+        tmp.close()
+        canvas = cv2.imread('slice.png')  # 用于数据显示的背景图片
+        print(result)
+        EMOTIONS = ["Anger", "Anticipation", "Disgust", "Fear", "Joy", "Sadness", "Surprise", "Trust"]
+
+        label = None  # 预测的标签
+        for (k, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+            # 用于显示各类别概率
+            text = "{}: {:.2f}%".format(emotion, prob * 100)
+            # 绘制表情类和对应概率的条形图
+            w = int(prob * 300) + 7
+            cv2.rectangle(canvas, (7, (k * 30) + 5), (w, (k * 30) + 30), (224, 200, 130), -1)
+            cv2.putText(canvas, text, (10, (k * 30) + 23), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 0, 0), k)
+        # 在显示结果的label中显示结果
+        
+        cv2.imwrite('new.png',canvas)
+        self.label_outputResult.setStyleSheet("border-image: url(new.png);")
+        self.label_scanResult.setText(result)
+        print(preds)
+
+    def real(self,opt, model, criterion, writer,image_path, optimizer,audio_path,i,log_dir):
+        spatial_transform = get_spatial_transform(opt, 'test')
+        temporal_transform = TSN(seq_len=opt.seq_len, snippet_duration=opt.snippet_duration, center=False)
+        target_transform = ClassLabel()
+        validation_data = get_validation_set(image_path,audio_path,opt, spatial_transform, temporal_transform, target_transform,i)
+        val_loader = get_data_loader(opt, validation_data, shuffle=False)
+
+        checkpoint = torch.load(log_dir,map_location=torch.device('cpu'))
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        result,preds = test(1, val_loader, model, criterion, opt, writer, optimizer)
+        t9 = Thread(target=self.show_emotion_picture,args=(result,preds))
+        t9.start()
+        
+
     def show_camera(self):
         # 定时器槽函数，每隔一段时间执行
-        flag, self.image = self.cap.read() # 获取画面
-        self.image=cv2.flip(self.image, 1) # 左右翻转
 
+        opt = parse_opts()
         tmp = open('slice.png', 'wb')
         tmp.write(b64decode(bgImg))
         tmp.close()
@@ -523,11 +626,48 @@ class Ui_MainWindow(QMainWindow):
         remove('slice.png')
 
         # 使用模型预测
-        #result = self.emotion_model.run(self.image, canvas, self.label_face, self.label_outputResult)
-        #self.logFile = '[' + self.timePlay + ']' + str(self.emotion_model.preds)
-        #logging.info(self.logFile)
-        # 在界面显示结果
-        #self.label_scanResult.setText(result)
+        
+        # 创建PyAudio对象
+        # 定义数据流块
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        # 录音时间
+        RECORD_SECONDS = 2
+
+        
+        i=0
+        j=-56
+        
+        p = pyaudio.PyAudio()
+        stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+        
+        while True:
+            tsk = []
+            i+=1
+            j+=56
+            t1 = Thread(target=record,args=(p,stream,i,CHUNK,FORMAT,CHANNELS,RECORD_SECONDS,RATE,self.audio_path,))
+            t2 = Thread(target=capture,args=(i,j,self.my_camera,self.images_path,))
+            tsk.append(t1)
+            tsk.append(t2)
+            t2.start()
+            t1.start()
+            for tt in tsk:
+                tt.join() 
+            #==================================================
+            self.real(opt, self.model, self.criterion, self.writer,self.image_path, self.optimizer,self.audio_path,i,self.log_dir)
+            #==================================================
+        stream.stop_stream()
+        stream.close()
+
+            # 关闭PyAudio
+        p.terminate()
+
 
 
 if __name__ == "__main__":  
